e0f78c062b386e5f8426328d5b8e67d1351ed7a1
Pavel Shatalov <p.sh.cpp@mail.ru>
2023-09-22 04:38:31 +0200

    finish 13.1.2

--- app.js
+++ app.js
@@ -1,33 +1,8 @@
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const { PORT = 3000 } = process.env;
-// // const { usersRouter } = require('./routes/users');
-// // const { cardsRouter } = require('./routes/cards');
-// // const PORT = 3000;
-// const app = express();
-
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
-// app.use(require('./routes/users'));
-// app.use(require('./routes/cards'));
-// // mongoose.connect('mongodb://localhost:27017/mestodb');
-// mongoose.connect('mongodb://localhost:27017/mestodb', { useNewUrlParser: true, useUnifiedTopology: true }, function(error) {
-//   if (error) {
-//     console.error('Ошибка подключения к MongoDB:', error);
-//   } else {
-//     console.log('Успешное подключение к MongoDB');
-//   }
-// });
-// app.listen(PORT, () => {
-//   console.log(`App listening on port ${PORT}`);
-// });
-
-
+// Description: точка входа в приложение  Express
 const express = require('express');
 const mongoose = require('mongoose');
-const { PORT = 3000 } = process.env;
+// const { PORT = 3000 } = process.env;
+const PORT = 3000;
 const app = express();
 
 app.use(express.json());
