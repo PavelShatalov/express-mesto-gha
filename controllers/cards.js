ea88ec5d8cd2a9060bb834ad02558634582bb751
Pavel Shatalov <p.sh.cpp@mail.ru>
2023-09-22 22:37:36 +0200

    finish 13.1.26

--- controllers/cards.js
+++ controllers/cards.js
@@ -5,7 +5,7 @@ module.exports.getCards = (req, res) => {
     res.status(404).send({ message: 'Карточки не найдены' });
   }
   Card.find({})
-    .then((cards) => res.send(cards))
+    .then((cards) => res.status(200).send(cards))
     .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 
 }; // возвращает все карточки
@@ -15,9 +15,24 @@ module.exports.createCard = (req, res) => {
     return res.status(400).send({ message: 'Некорректные данные' });
   }
   Card.create({ name, link })
-    .then((card) => res.status(200).send({ id: card._id })) //
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .then((card) => res.status(200).send({
+      likes: card.likes,
+      _id: card._id,
+      name: card.name,
+      link: card.link,
+      owner: {
+        name: card.owner.name,
+        about: card.owner.about,
+        avatar: card.owner.avatar,
+        _id: card.owner._id,
+      }
+
+    })) //
+    .catch(() => res.status(400).send({ message: 'Произошла ошибка' }));
 };
+
+
+
 // создаёт карточку с переданными в теле запроса name и link
 module.exports.deleteCard = (req, res) => {
   const { cardId } = req.params;
@@ -35,16 +50,9 @@ module.exports.deleteCard = (req, res) => {
     .catch(() => res.status(400).send({ message: 'Произошла ошибка' }));
 }; // удаляет карточку по _id
 
-
-
 module.exports.likeCard = (req, res) => {
   const { cardId } = req.params;
   const { _id: userId } = req.user;
-
-  // Проверка на корректность ObjectId
-  // if (!cardId || !userId) {
-  //   return res.status(400).send({ message: 'Передан некорректный _id карточки.' });
-  // }
 
   Card.findByIdAndUpdate(
     cardId,
@@ -65,14 +73,13 @@ module.exports.likeCard = (req, res) => {
       res.status(400).send({ message: 'Ошибка сервера. Попробуйте позже.' });
     });
 };
+
+
+
 module.exports.dislikeCard = (req, res) => {
   const { cardId } = req.params;
   const { _id: userId } = req.user;
 
-  // Проверка на корректность ObjectId
-  // if (!cardId || !userId) {
-  //   return res.status(400).send({ message: 'Передан некорректный _id карточки.' });
-  // }
 
   Card.findByIdAndUpdate(
     cardId,
