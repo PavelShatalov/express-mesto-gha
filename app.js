96944f61aab4ea630e3cc4408d056c6b906b98e1
Pavel Shatalov <p.sh.cpp@mail.ru>
2023-10-01 01:36:27 +0200

    finish 14.2.3

--- app.js
+++ app.js
@@ -13,6 +13,15 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Успешное подключение к MongoDB');
+    app.listen(PORT, () => {
+      console.log(`App listening on port ${PORT}`);
+    });
+  })
+  .catch((error) => { console.error('Ошибка подключения к MongoDB:', error); });
+
 // app.post('/signin', login);
 // app.post('/signup', createUser);
 app.post('/signin', validation.login, login);
@@ -22,12 +31,3 @@ app.use(routes);
 
 app.use(errors());
 app.use(centralError);
-
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Успешное подключение к MongoDB');
-    app.listen(PORT, () => {
-      console.log(`App listening on port ${PORT}`);
-    });
-  })
-  .catch((error) => { console.error('Ошибка подключения к MongoDB:', error); });
